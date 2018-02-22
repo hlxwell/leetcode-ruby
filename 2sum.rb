aeb3189f42154f51fd3d0df64a49d751b2a11eee
Michael He <hlxwell@gmail.com>
2018-02-23 02:22:46 +0900

    update 2 sum

--- 2sum.rb
+++ 2sum.rb
@@ -2,13 +2,13 @@ require 'benchmark'
 require "pp"
 
 
-### RUBY WAY ------------------------------------------------
-def two_sum(nums, target)
-  Hash[(0...nums.size).to_a.combination(2).to_a.zip(nums.combination(2))].each do |k, v|
-    return k if v.inject(0) {|total, n| total += n} == target
-  end
-end
-pp two_sum([2,3,5,7,9,11,4], 12) # [1, 4]
+# ### RUBY WAY ------------------------------------------------
+# def two_sum(nums, target)
+#   Hash[(0...nums.size).to_a.combination(2).to_a.zip(nums.combination(2))].each do |k, v|
+#     return k if v.inject(0) {|total, n| total += n} == target
+#   end
+# end
+# pp two_sum([2,3,5,7,9,11,4], 12) # [1, 4]
 
 
 ### FAST WAY ------------------------------------------------
@@ -17,9 +17,11 @@ def two_sum(nums, target)
   nums.each_with_index do |num, i|
     matched_num_index = num_with_index[target - num]
     if !matched_num_index.nil?
-      return [i, matched_num_index]
+      return [matched_num_index, i]
     end
     num_with_index[num] = i
   end
 end
+
 pp two_sum([2,3,5,7,9,11,4], 12) # [1, 4]
+# pp two_sum([3,2,4], 6) # [1, 2]
