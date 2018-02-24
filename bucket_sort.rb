0172e759d46599725a3cc598f0c85452bcf67f92
Michael He <hlxwell@gmail.com>
2018-02-25 00:36:52 +0900

    add bucket sort

---
+++ bucket_sort.rb
@@ -0,0 +1,23 @@
+arr = [9,1,8,2,7,3,6,4,5,0,10]
+
+def bucket_sort(arr)
+  result = []
+  max = arr.max
+  arr_size = arr.size
+  bucket = Array.new(arr_size) { [] }
+  
+  arr.each_with_index do |n, i|
+    bucket_index = n * arr_size / (max + 1)
+    bucket[bucket_index] << n
+  end
+
+  bucket.each do |list|
+    list.each do |n|
+      result << n
+    end
+  end
+
+  result
+end
+
+p bucket_sort arr
