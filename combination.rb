5fc0274099b1a0692b693681a604ae3cae1360ad
Michael He <hlxwell@gmail.com>
2018-03-08 14:45:09 +0900

    use global var instead of passing var to simplify the code. also add comments to help remembering the process.

--- combination.rb
+++ combination.rb
@@ -1,18 +1,37 @@
+# Notes
+# [1,2,3]的子问题是
+
+# 递归过程
+# 1层. [] 与 [1, 2, 3] 每个子元素组合
+# 2层. [1] 与 [2, 3] 每个子元素组合
+#      [2] 与 [3] 每个子元素组合
+
 # def combine n, k
 #   (0..n).to_a.combination(k).to_a
 # end
 
 def combine(n, k)
   @result = []
-  dfs (1..n).to_a, [], k, 0
+  @nums = (1..n).to_a
+  @comb_size = k
+  dfs [], 0
   @result
 end
 
-def dfs nums, tmp, comb_size, index
-  @result << tmp if tmp.size == comb_size
-  index.upto(nums.size - 1) do |i|
-    dfs nums, tmp + [nums[i]], comb_size, i + 1
+# (base arr) combine all elements in [index..end]
+def dfs(sub_arr, index)
+  # 结束条件
+  if sub_arr.size == @comb_size
+    @result << sub_arr
+    return
+  end
+
+  # 找出子问题继续循环
+  index.upto(@nums.size - 1) do |i|
+    j = i + 1
+    dfs sub_arr + [@nums[i]], j
   end
 end
 
-p combine 10, 1
+p combine 3, 2
+
