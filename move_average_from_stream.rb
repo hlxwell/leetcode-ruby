f778f156bc04b4632c4a5d7e8d479b523a9b339a
Michael He <hlxwell@gmail.com>
2019-08-11 00:23:36 +0900

    improve the move avergage from stream solution

--- move_average_from_stream.rb
+++ move_average_from_stream.rb
@@ -1,14 +1,21 @@
 class MovingAverage
-  def initialize window_size
+  def initialize(window_size)
     @window_size = window_size
+    @current_sum = 0
     @queue = []
   end
 
   def next(num)
     @queue.push num
-    @queue.shift if @queue.size > @window_size
-    total = @queue.inject(0) { |sum, n| sum += n }
-    total / @queue.size
+    @current_sum += num
+    if @queue.size > @window_size
+      @current_sum -= @queue.shift
+    end
+
+    ### slow solution
+    # total = @queue.inject(0) { |sum, n| sum += n }
+    # total / @queue.size
+    @current_sum / @queue.size
   end
 end
 
