9eafa65edf3dbc1ed2d90b8171c5726570a04810
Michael He <hlxwell@gmail.com>
2019-11-18 15:52:37 +0900

    Update adjacent_values.rb
    improve from n^3 to n

--- adjacent_values.rb
+++ adjacent_values.rb
@@ -1,24 +1,22 @@
-def adjacent_value arr
-    result = -1
-    cache = {}
-    arr.each_with_index do |item, i|
-        cache[item] ||= []
-        cache[item] << i
-    end
+# frozen_string_literal: true
 
-    sorted_uniq_keys = cache.keys.sort
+def adjacent_value(arr)
+  result = -1
+  cache = {}
+  arr.each_with_index do |item, i| # O(n)
+    cache[item] ||= []
+    cache[item] << i
+  end
+  sorted_uniq_keys = cache.keys.sort # O(n*ln(n))
+  0.upto(sorted_uniq_keys.size - 2) do |i| # O(n)
+    a = cache[sorted_uniq_keys[i]]
+    b = cache[sorted_uniq_keys[i + 1]]
+    result = (a[0] - b[-1]).abs if (a[0] - b[-1]).abs > result
+    result = (a[-1] - b[0]).abs if (a[-1] - b[0]).abs > result
+  end
 
-    0.upto(sorted_uniq_keys.size - 2) do |i|
-        cache[sorted_uniq_keys[i]].each do |a|
-            cache[sorted_uniq_keys[i + 1]].each do |b|
-                if (a-b).abs > result
-                    result = (a-b).abs
-                end
-            end
-        end
-    end
-    result
+  result
 end
 
-puts adjacent_value [0,3,3,7,5,3,11,1] # => 7
-puts adjacent_value [1,4,7,3,3,5] # => 4
+puts adjacent_value [0, 3, 3, 7, 5, 3, 11, 1] # => 7
+puts adjacent_value [1, 4, 7, 3, 3, 5] # => 4
