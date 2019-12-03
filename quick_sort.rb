e7f73fd52a426a9705e15196d2e50d5a2262b323
Michael He <hlxwell@gmail.com>
2019-12-03 10:19:25 +0900

    Add quick sort

---
+++ quick_sort.rb
@@ -0,0 +1,43 @@
+def quick_sort arr
+  return arr if arr.size <= 1
+  if arr.size == 2
+    if arr[0] > arr[1]
+      arr[0], arr[1] = arr[1], arr[0]
+    end
+    return arr
+  end
+
+  pivot_i = arr.size - 1
+  left_i = 0
+  right_i = pivot_i - 1
+
+  while left_i < right_i
+    left_i.upto(pivot_i) do |i|
+      left_i = i
+      break if arr[left_i] > arr[pivot_i]
+    end
+
+    right_i.downto(left_i) do |j|
+      right_i = j
+      break if arr[right_i] < arr[pivot_i]
+    end
+
+    arr[left_i], arr[right_i] = arr[right_i], arr[left_i]
+  end
+
+  # 1. left == right
+  # 2. left == pivot
+  if arr[left_i] > arr[pivot_i]
+    arr[left_i], arr[pivot_i] = arr[pivot_i], arr[left_i]
+  end
+
+  pivot_i = left_i
+  left_side = arr[0..(pivot_i-1)]
+  right_side = arr[(pivot_i+1)..(arr.size-1)]
+
+  quick_sort(left_side) + [arr[pivot_i]] + quick_sort(right_side)
+end
+
+arr = rand(10).times.map { rand(100) }
+puts arr.inspect
+puts quick_sort(arr).inspect
