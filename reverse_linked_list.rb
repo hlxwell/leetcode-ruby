07881d1687b39d228d4a7f5334a8ba8d239f1c87
Michael He <hlxwell@gmail.com>
2019-12-08 03:51:59 +0900

    optimize the reverse linked list

--- reverse_linked_list.rb
+++ reverse_linked_list.rb
@@ -1,17 +1,20 @@
 require_relative "lib/linked_list_helper"
 
 def reverse_linked_list(root)
-  prev_node = new_head = nil
-  node = root
-  while node
-    next_node = node.next
-    node.next = prev_node
-    prev_node = node
-    new_head = node if next_node.nil? # detect if it's last node
-    node = next_node
+  return root if root.nil? or root.next.nil?
+  current = root
+  prev = nil
+  post = current.next
+
+  while current
+    current.next = prev # point to prev
+    break if post.nil?
+    # shift, prev, current, post
+    prev, current, post = current, post, post.next
   end
-  new_head
+  current
 end
 
 head = arr_to_linked_list [1, 2, 3, 4, 5, 6, 7]
 p reverse_linked_list(head)
+
