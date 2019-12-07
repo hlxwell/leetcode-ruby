3e911e60ca1e171efb0adcbea56b8928ae56014c
Michael He <hlxwell@gmail.com>
2019-12-07 21:13:52 +0900

    change the reverse linked list to inplace reversing.

--- reverse_linked_list.rb
+++ reverse_linked_list.rb
@@ -1,19 +1,17 @@
-require_relative 'lib/linked_list_helper'
+require_relative "lib/linked_list_helper"
 
-def reverse_linked_list root
-  new_root = nil
-
+def reverse_linked_list(root)
+  prev_node = new_head = nil
   node = root
   while node
-    new_node = node.clone
-    new_node.next = new_root
-    new_root = new_node
-
-    node = node.next
+    next_node = node.next
+    node.next = prev_node
+    prev_node = node
+    new_head = node if next_node.nil? # detect if it's last node
+    node = next_node
   end
-
-  new_root
+  new_head
 end
 
-head = arr_to_linked_list [1,2,3,4,5,6,7]
-p reverse_linked_list head
+head = arr_to_linked_list [1, 2, 3, 4, 5, 6, 7]
+p reverse_linked_list(head)
