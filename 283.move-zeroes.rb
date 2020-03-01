d2459cb598123cd1c512d5f59005d0474ca6c703
Michael He <hlxwell@gmail.com>
2020-03-01 18:08:32 +0900

    change 283 solution file

--- 283.move-zeroes.rb
+++ 283.move-zeroes.rb
@@ -1,3 +1,12 @@
+#
+# @lc app=leetcode id=283 lang=ruby
+#
+# [283] Move Zeroes
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
 def move_zeroes(nums)
   0.upto(nums.size - 2) do |i|
     next if nums[i] != 0
@@ -11,4 +20,4 @@ def move_zeroes(nums)
   nums
 end
 
-puts move_zeroes([0, 1, 0, 3, 0, 12])
+# @lc code=end
