24dfd4d7ed6d3f25b7428d377f8f3a3ffd6bd634
Michael He <hlxwell@gmail.com>
2020-03-01 17:52:06 +0900

    add a reverse method from scratch

---
+++ reverse_string.rb
@@ -0,0 +1,11 @@
+# in-place reverse a string
+def reverse_string(s)
+  0.upto(s.size / 2 - 1) do |i|
+    s[i], s[-i - 1] = s[-i - 1], s[i]
+  end
+  s
+end
+
+s = "a b  cd efg"
+reverse_string s
+puts s
