7d07af90fab241acffb3ef91f84b7e4d6b579cf3
Michael He <michael.he@bytedance.com>
2021-01-23 21:54:39 +0900

    add expand array solution

---
+++ expand_subarry.rb
@@ -0,0 +1,23 @@
+@solutions = []
+def iterate_arr arr
+  if arr.none? { |elem| elem.is_a? Array }
+    @solutions << arr
+    return
+  end
+
+  # byebug
+  arr.each_with_index do |elem, i|
+    if elem.is_a? Array
+      new_arr = arr.dup
+      elem.each do |n|
+        new_arr[i] = n
+
+        iterate_arr(new_arr)
+      end
+      return
+    end
+  end
+end
+
+iterate_arr [1,2,[3,4],5,[6,7]]
+pp @solutions
